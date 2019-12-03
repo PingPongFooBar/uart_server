5f4fd9bcf59d95cc4d0143b7f0eebfd6b8ef7019
chenhui-linux <58462835+chenhui-linux@users.noreply.github.com>
2019-12-03 16:52:13 +0800

    Update oc_protect.c

--- junc_server/oc_protect.c
+++ junc_server/oc_protect.c
@@ -1,1 +1,29 @@
-#include <fcntl.h>#include <stdlib.h>#include <unistd.h>#include <stdio.h>#include <signal.h>#include <string.h>#include "junction_app.h"int fd;char path[20];void oc_handler(int num){	int i;	ioctl(fd,OFF);}/* ²ÎÊı1 ÓÃÀ´±íÊ¾µÚ¼¸¸öÉè±¸ */int main(int argc, char **argv){	int oflag;	snprintf(path,20,"/dev/junction%s",argv[1]);	fd = open(path,O_RDWR);	signal(SIGIO,oc_handler);	fcntl(fd, F_SETOWN, getpid());	oflag = fcntl(fd, F_GETFL);	fcntl(fd, F_SETFL, oflag | FASYNC);	while(1);	return 0;	}
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include "junction_app.h"
+
+int fd;
+char path[20];
+void oc_handler(int num){
+	int i;
+	ioctl(fd,OFF);
+}
+
+/* å‚æ•°1 ç”¨æ¥è¡¨ç¤ºç¬¬å‡ ä¸ªè®¾å¤‡ */
+int main(int argc, char **argv)
+{
+	int oflag;
+	snprintf(path,20,"/dev/junction%s",argv[1]);
+	fd = open(path,O_RDWR);
+	signal(SIGIO,oc_handler);
+	fcntl(fd, F_SETOWN, getpid());
+	oflag = fcntl(fd, F_GETFL);
+	fcntl(fd, F_SETFL, oflag | FASYNC);
+
+	while(1);
+	return 0;	
+}
